6c9afb5057a73e6dce7cd03a664e8d5c6ba7d310
Orçun Çolak <orcun.colak@hazelcast.com>
2024-02-08 16:28:23 +0300

    show charset and collation

---
+++ charset-collations/README.md
@@ -0,0 +1,32 @@
+# Read me
+
+The original information is from
+https://planetscale.com/blog/mysql-charsets-collations
+
+This shows charset
+
+```sql
+SHOW CREATE TABLE tablename;
+```
+
+The output is
+> CREATE TABLE `all_char_types_table` (
+> ...
+> ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+# utf8mb4
+
+utf8mb4 means that it is actually UTF-8
+
+# utf8mb4_0900_ai_ci
+
+It is **Accent-insensitive** and **Case-insensitive**
+
+| Suffix | Meaning            |
+|--------|--------------------|
+| _ai    | Accent-insensitive |
+| _as    | Accent-sensitive   |
+| _ci    | Case-insensitive   |
+| _cs    | Case-sensitive     |
+| _ks    | Kana-sensitive     |
+| _bin   | Binary             |
--- docker-compose/columntypes/all-types/initial-data.sql
+++ docker-compose/columntypes/all-types/initial-data.sql
@@ -1,6 +1,6 @@
 -- https://blog.devart.com/mysql-data-types.html
 -- https://blog.devart.com/mysql-int-data-type.html#what_is_mysql_integer
-create TABLE all_char_types_table (
+CREATE TABLE all_char_types_table (
     varchar_column VARCHAR(255),
     char_column CHAR(10),
     text_column TEXT,
@@ -9,45 +9,45 @@ create TABLE all_char_types_table (
     longtext_column LONGTEXT
 );
 
-create TABLE all_blob_types_table (
+CREATE TABLE all_blob_types_table (
     blob_column BLOB,
     tinyblob_column TINYBLOB,
     mediumblob_column MEDIUMBLOB,
     longblob_column LONGBLOB
 );
 
-create TABLE all_binarychar_types_table (
+CREATE TABLE all_binarychar_types_table (
     binary_column BINARY(10),
     varbinary_column VARBINARY(255)
 );
 
 
-create TABLE enum_type_table (
+CREATE TABLE enum_type_table (
     size enum ('small', 'medium', 'large') NOT NULL
 );
 
-create TABLE integer_types_table (
+CREATE TABLE integer_types_table (
     tinyint_signed TINYINT,
     tinyint_unsigned TINYINT UNSIGNED, -- Between 0 and 255. Java uses short
     smallint_signed SMALLINT,
-    smallint_unsigned SMALLINT UNSIGNED, --Between 0 and 65535. Java uses Integer
+    smallint_unsigned SMALLINT UNSIGNED, -- Between 0 and 65535. Java uses Integer
     mediumint_signed MEDIUMINT, -- Between -8388608 and 8388607. Java uses Integer
-    mediumint_unsigned MEDIUMINT UNSIGNED, -- Betweeen 0 and 16777215. Java uses Integer
+    mediumint_unsigned MEDIUMINT UNSIGNED, -- Between 0 and 16777215. Java uses Integer
     int_signed INT,
     int_unsigned INT UNSIGNED, --Between 0 and 4294967295. Java uses Long
     bigint_signed BIGINT,
-    bigint_unsigned BIGINT UNSIGNED -- Betweeen 0 and 18446744073709551615. Java uses BigInteger
+    bigint_unsigned BIGINT UNSIGNED -- Between 0 and 18446744073709551615. Java uses BigInteger
 );
 
-create TABLE boolean_types_table (
+CREATE TABLE boolean_types_table (
     is_active BOOLEAN -- You can use BOOLEAN or TINYINT(1)
 );
 
-create TABLE bit_types_table (
+CREATE TABLE bit_types_table (
     bit_column BIT(8)
 );
 
-create TABLE all_numeric_types_table (
+CREATE TABLE all_numeric_types_table (
     decimal_signed DECIMAL(10,2),
     decimal_unsigned DECIMAL(10,2) UNSIGNED,
     numeric_signed NUMERIC(8,4),
@@ -58,7 +58,7 @@ create TABLE all_numeric_types_table (
     double_unsigned DOUBLE(8,4) UNSIGNED
 );
 
-create TABLE datetime_types_table (
+CREATE TABLE datetime_types_table (
     -- Stores date values in the format 'YYYY-MM-DD'.
     date_column DATE,
     --  Stores time values in the format 'HH:MM:SS'.
